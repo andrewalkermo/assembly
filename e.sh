0effbb3c2f2637cfdcd5d2d987b821e2a47c8a59
André Walker <andre.walkermo@gmail.com>
2019-03-24 16:19:12 -0300

    e.sh aceia arquivo com ou sem extensão

--- e.sh
+++ e.sh
@@ -3,12 +3,11 @@
 if [[ -z $1 ]]; then
   read -p "Digite o nome arquivo sem extencao: " entrada
 else
-  entrada=$1
+  entrada="$(cut -d'.' -f 1 <<< $1)"
 fi
 cp ${entrada}.asm ./nasm &&
 cd nasm &&
 nasm -f elf32 ${entrada}.asm &&
 gcc -m32 -o ${entrada} ${entrada}.o driver.c asm_io.o &&
 ./${entrada} &&
-# rm ${entrada}* &&
 cd ..
