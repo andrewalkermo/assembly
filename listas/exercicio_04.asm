d8623fe2eb66756bc04ae471b5e47bf40e7bbffa
André <andre.walkermo@gmail.com>
2019-04-08 15:13:27 -0300

    comeca q4

--- listas/exercicio_04.asm
+++ listas/exercicio_04.asm
@@ -6,19 +6,27 @@ printpar db    "par", 0
 printimpar db    "impar", 0
 
 segment .bss
-palavra
+palavra     resb    10
 segment .text
         global  asm_main
 asm_main:
         enter   0,0
         pusha
+        mov     ecx, 9
+        leitura:
+            call read_char
+            mov [palavra+ecx], al
+            loop leitura
 
-        call read_int
+        mov ecx, 9
+        mov ebx, 0
 
-
-
-
-
+        saida:
+            mov eax, [palavra+ebx]
+            call print_char
+            call print_nl
+            inc ebx
+            loop saida
         popa
         leave
         ret
